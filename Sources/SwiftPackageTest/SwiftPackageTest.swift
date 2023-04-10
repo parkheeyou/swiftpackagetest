d55c8a2e422cba2435cc3e17e0277d7c21c2de08
hugh <metalzzang@gmail.com>
2023-04-10 11:38:39 +0900

    Update SwiftPackageTest.swift
    utils test

--- Sources/SwiftPackageTest/SwiftPackageTest.swift
+++ Sources/SwiftPackageTest/SwiftPackageTest.swift
@@ -1,6 +1,8 @@
 public struct SwiftPackageTest {
     public private(set) var text = "Hello, World!"
+    public var commonUtils: CommonUtils
 
     public init() {
+        commonUtils = CommonUtils()
     }
 }
